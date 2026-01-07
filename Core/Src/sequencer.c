e8f6fefad6792b6ea1a4543c1cc8e68f745e1283
Gok005 <gokulspgt@gmail.com>
2026-01-07 16:24:00 +0530

    add central event-group-based sequencing controller

---
+++ Core/Src/sequencer.c
@@ -0,0 +1,78 @@
+/**
+ * @file sequencer.c
+ * @brief Central transplant sequencing logic
+ */
+
+#include "sequencer.h"
+#include "servo_task.h"
+#include "pickup_task.h"
+#include "leadscrew_task.h"
+#include "conveyor_task.h"
+#include "task_config.h"
+
+/* RTOS objects */
+TaskHandle_t SequencerTaskHandle = NULL;
+EventGroupHandle_t SystemEventGroup = NULL;
+
+void SequencerTask(void *argument)
+{
+    uint8_t cycleCount = 0;
+
+    /* Create event group */
+    SystemEventGroup = xEventGroupCreate();
+
+    for (;;)
+    {
+        /* ---- Planting phase ---- */
+
+        /* 1. Close servo */
+        Servo_RequestClose();
+        xEventGroupWaitBits(SystemEventGroup,
+                            EVT_SERVO_DONE,
+                            pdTRUE,
+                            pdTRUE,
+                            portMAX_DELAY);
+
+        /* 2. Pickup forward + leadscrew start */
+        Pickup_RequestForward();
+        Leadscrew_RequestStart();
+
+        xEventGroupWaitBits(SystemEventGroup,
+                            EVT_PICKUP_DONE | EVT_LEADSCREW_DONE,
+                            pdTRUE,
+                            pdTRUE,
+                            portMAX_DELAY);
+
+        /* 3. Open servo */
+        Servo_RequestOpen();
+        xEventGroupWaitBits(SystemEventGroup,
+                            EVT_SERVO_DONE,
+                            pdTRUE,
+                            pdTRUE,
+                            portMAX_DELAY);
+
+        /* 4. Pickup retract (leadscrew idle) */
+        Pickup_RequestBackward();
+        xEventGroupWaitBits(SystemEventGroup,
+                            EVT_PICKUP_DONE,
+                            pdTRUE,
+                            pdTRUE,
+                            portMAX_DELAY);
+
+        cycleCount++;
+
+        /* ---- Batch handling ---- */
+        if (cycleCount >= TRANSPLANT_CYCLE_COUNT)
+        {
+            Conveyor_RequestMove();
+            xEventGroupWaitBits(SystemEventGroup,
+                                EVT_CONVEYOR_DONE,
+                                pdTRUE,
+                                pdTRUE,
+                                portMAX_DELAY);
+
+            Leadscrew_ReverseDirection();
+            cycleCount = 0;
+        }
+    }
+}
