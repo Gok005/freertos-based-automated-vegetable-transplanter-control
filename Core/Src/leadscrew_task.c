67985dda0828b746e6249266c3f2f3ca8e60f438
Gok005 <gokulspgt@gmail.com>
2026-01-07 16:19:18 +0530

    add leadscrew task skeleton with direction control interface

---
+++ Core/Src/leadscrew_task.c
@@ -0,0 +1,65 @@
+/**
+ * @file leadscrew_task.c
+ * @brief FreeRTOS task for leadscrew motor control
+ */
+
+#include "leadscrew_task.h"
+
+/* RTOS objects */
+TaskHandle_t LeadscrewTaskHandle = NULL;
+
+/* Internal state */
+static system_state_t currentState = SYSTEM_INIT;
+
+/* Control flags */
+static volatile uint8_t leadscrewRunRequested = 0;
+static volatile uint8_t leadscrewStopRequested = 0;
+static volatile uint8_t leadscrewReverseRequested = 0;
+
+void LeadscrewTask(void *argument)
+{
+    for (;;)
+    {
+        /* Block until a control request is received */
+        ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+
+        if (leadscrewReverseRequested)
+        {
+            /* TODO: Reverse motor direction */
+            leadscrewReverseRequested = 0;
+            currentState = LEADSCREW_DIRECTION_CHANGE;
+        }
+
+        if (leadscrewRunRequested)
+        {
+            /* TODO: Start leadscrew motion */
+            leadscrewRunRequested = 0;
+        }
+
+        if (leadscrewStopRequested)
+        {
+            /* TODO: Stop leadscrew motion */
+            leadscrewStopRequested = 0;
+        }
+    }
+}
+
+/* API used by other tasks */
+
+void Leadscrew_RequestStart(void)
+{
+    leadscrewRunRequested = 1;
+    xTaskNotifyGive(LeadscrewTaskHandle);
+}
+
+void Leadscrew_RequestStop(void)
+{
+    leadscrewStopRequested = 1;
+    xTaskNotifyGive(LeadscrewTaskHandle);
+}
+
+void Leadscrew_ReverseDirection(void)
+{
+    leadscrewReverseRequested = 1;
+    xTaskNotifyGive(LeadscrewTaskHandle);
+}
